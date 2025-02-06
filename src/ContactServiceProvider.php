0cf38f847ae8afa6265c3ace6b25e78921c8d5b0
Mohsin-Imran <mohsin.centosquare@gmail.com>
2025-02-06 23:33:14 +0500

    Updated package with new features

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -8,15 +8,14 @@ class ContactServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Load routes, views, migrations, etc.
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'crud');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-
+    
         // Publish views
         $this->publishes([
             __DIR__ . '/views' => resource_path('views/vendor/mohsincrud'),
-        ], 'views');
+        ], 'views'); 
     }
 
     public function register()
